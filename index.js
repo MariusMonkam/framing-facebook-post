366f66418575072f1a8dbbbc9ab84f69ac11f31f
MariusMonkam <cloudmonde@yahoo.com>
2019-10-01 21:41:30 +0300

    3 files modified

--- index.js
+++ index.js
@@ -4,17 +4,38 @@ import $ from "jquery";
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>Facebook post template</h1>`;
-var images =['https://via.placeholder.com/500/0000FF/808080 ?Text=image1',
-'https://via.placeholder.com/500/FF0000/FFFFFF?Text=image2','https://via.placeholder.com/500/FFFF00/000000?Text=image3',
-'https://via.placeholder.com/500/000000/FFFFFF?Text=image4']
+appDiv.innerHTML = `<h1>Click callback Facebook sized image </h1>`;
+var images =['https://via.placeholder.com/500',
+'https://via.placeholder.com/500/FF0000/FFFFFF?Text=image1','https://via.placeholder.com/500/FFFF00/000000?Text=image',
+'https://via.placeholder.com/500/000000/FFFFFF?Text=image']
 var playing= false;
 $(function(){
 //are we playing 
 //no --> show message bord with
 $("#footContainer").show()
-$("#message").show()
  //display the message "MESSAGE BOARD"
+
+ $("#message").show()
+ setTimeout(function(){
+$("#message").html("Click On Me")
+},3000);
+//click on message board
+
+ $("#message").click(function(){
+  
+   $("#message").hide()
+   $("#footContainer").hide()
+   $("#imageContainer").show()
+  setTimeout(function(){
+  $("#imageContainer").append(`<img src="https://via.placeholder.com/615X595/0000FF/808080 ?Text=Digital.com" id="image1" class="image">`)
+
+ },1000)
+
+ })
+
+
+ 
+ 
 // yes -->
 //  reload the page
 // show the image carosselle with the text loading...
